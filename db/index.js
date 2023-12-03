f2c240f1ffae3ecd604d3eef50f459ffe0796c7f
yuriy-npm <gnatko.yr@gmail.com>
2023-12-03 18:18:53 +0200

    configure db

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+require("dotenv").config();
+
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -8,69 +9,65 @@ const sequelize = new Sequelize(
 
   {
     dialect: "postgres",
+
+    dialectModule: require('pg'),
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, 
- 
+    port: process.env.DB_PORT,
+
     dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-    
-    }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
-
-
-const Product = require('./Product')(sequelize) 
-const Category = require('./Category')(sequelize)
-const User = require('./User')(sequelize)
-const Cart = require('./Cart')(sequelize)
-const Favourite = require('./Favourite')(sequelize)
-const Order = require('./Order')(sequelize)
-const Address = require('./Address')(sequelize)
-const Order_Item = require('./Order_Item')(sequelize)
+const Product = require("./Product")(sequelize);
+const Category = require("./Category")(sequelize);
+const User = require("./User")(sequelize);
+const Cart = require("./Cart")(sequelize);
+const Favourite = require("./Favourite")(sequelize);
+const Order = require("./Order")(sequelize);
+const Address = require("./Address")(sequelize);
+const Order_Item = require("./Order_Item")(sequelize);
 
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
-User.hasMany(Cart)
-Cart.belongsTo(User)
+User.hasMany(Cart);
+Cart.belongsTo(User);
 
-Product.hasMany(Cart)
-Cart.belongsTo(Product)
+Product.hasMany(Cart);
+Cart.belongsTo(Product);
 
-User.hasMany(Favourite)
-Favourite.belongsTo(User)
+User.hasMany(Favourite);
+Favourite.belongsTo(User);
 
-User.hasMany(Order)
-Order.belongsTo(User)
+User.hasMany(Order);
+Order.belongsTo(User);
 
-Product.hasMany(Favourite)
-Favourite.belongsTo(Product)
+Product.hasMany(Favourite);
+Favourite.belongsTo(Product);
 
-User.hasMany(Address)
-Address.belongsTo(User)
+User.hasMany(Address);
+Address.belongsTo(User);
 // ------------------------
 
-Order.hasMany(Order_Item)
-Order_Item.belongsTo(Order)
+Order.hasMany(Order_Item);
+Order_Item.belongsTo(Order);
 
-Product.hasMany(Order_Item)
-Order_Item.belongsTo(Product)
-
-
-
+Product.hasMany(Order_Item);
+Order_Item.belongsTo(Product);
 
 module.exports = {
-    sequelize: sequelize,
-    Product,
-    Category,
-    User,
-    Cart, 
-    Favourite,
-    Order,
-    Address,
-    Order_Item,
-}
+  sequelize: sequelize,
+  Product,
+  Category,
+  User,
+  Cart,
+  Favourite,
+  Order,
+  Address,
+  Order_Item,
+};
